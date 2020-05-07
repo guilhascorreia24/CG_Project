a88046c44764c29738907a1a9140e1177b101b70
andrebarrocas <andrebarrocas99@hotmail.com>
2020-05-07 21:39:03 +0100

    rip

--- World.cpp
+++ World.cpp
@@ -1,5 +1,5 @@
 #include "World.h"
-#define SUN_GRAVITY 1
+
 
 void World::addObject(Object* obj){
     objects.push_back(obj);
@@ -10,19 +10,15 @@ World::World() : main(0){
 }
 
 void World::fillObjects(){
-    Point nave_point(0,0,-25);
-    Point planet_point(10,30,-100);
-    Point satelite_point(30,8,-50);
-    Point sol_point(0,0,0);
-
+    Point nave_point(0,0,0);
 
     Point disco_nave_point(5,0,0);
     Point cupula_nave_point(2,10,0);
     Point base_nave_point(0,-5,0);
 
-    /*Point planet_point(10,10,-50);
+    Point planet_point(10,10,-50);
     Point satelite_point(30,8,-50);
-    Point sol_point(0,0,-200);*/
+    Point sol_point(0,0,-200);
 
     Point asteroide1_point(50,0,-50);
     Point asteroide2_point(-50,0,-50);
@@ -32,59 +28,47 @@ void World::fillObjects(){
 
     Object* disco_nave = new DiscoNave(nave_point);
     disco_nave->setPosition(disco_nave_point);
-    disco_nave->setVelocity(0.f);
+    disco_nave->setVelocity(0.001);
 
     Object* cupula_nave = new CupulaNave(nave_point);
     cupula_nave->setPosition(cupula_nave_point);
-    cupula_nave->setVelocity(0.f);
+    cupula_nave->setVelocity(0.001);
 
     Object* base_nave = new BaseNave(nave_point);
     base_nave->setPosition(base_nave_point);
-    base_nave->setVelocity(0.f);
+    base_nave->setVelocity(0.001);
 
     Object* sol = new Sol(nave_point);
     sol->setPosition(sol_point);
-    sol->setVelocity(0.f);
-    sol->setSize(5);
-    sol->setMass(10000);
+    sol->setVelocity(0.0);
+    sol->setSize(15);
     
     Object* planeta = new Planeta(nave_point);
     planeta->setPosition(planet_point);
-    planeta->setVelocity(0.f);
-    planeta->setMass(1000);
-    planeta->applyGravityPull(sol);
-
+    planeta->setVelocity(0.0);
 
     Object* satelite = new Satelite(planet_point);
     satelite->setPosition(satelite_point);
-    satelite->setVelocity(0.f);
-    satelite->setMass(10);
-    satelite->applyGravityPull(sol);
-
-    satelite->setForceToOrbit(sol,1.55);
-    planeta->setForceToOrbit(sol,1.55);
-
-    satelite->setVelocity(0.1);
-
+    satelite->setVelocity(0.001);
 
 //--------------------------------------------------------------------
 
     Object* asteroide1 = new Asteroide1(sol_point);
     asteroide1->setPosition(asteroide1_point);
-    asteroide1->setVelocity(0.0f);
+    asteroide1->setVelocity(0.01);
 
     Object* asteroide2 = new Asteroide1(sol_point);
     asteroide2->setPosition(asteroide2_point);
-    asteroide2->setVelocity(0.0f);
+    asteroide2->setVelocity(0.01);
 
 
     Object* asteroide3 = new Asteroide2(sol_point);
     asteroide3->setPosition(asteroide3_point);
-    asteroide3->setVelocity(0.0f);
+    asteroide3->setVelocity(0.01);
 
     Object* asteroide4 = new Asteroide2(sol_point);
     asteroide4->setPosition(asteroide4_point);
-    asteroide4->setVelocity(0.0f);
+    asteroide4->setVelocity(0.01);
 
 
 
@@ -103,7 +87,7 @@ void World::fillObjects(){
     addObject(asteroide4);
 
     //addObject(nave);
-    main = sol;
+    //main = nave;
 }
 
 World::~World(){
@@ -125,6 +109,6 @@ void World::draw(){
 
 void World::update(){
     for(Object * object : objects){
-        object->update(getTimeInMili());
+        object->update();
     }
 }
