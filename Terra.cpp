b321727f4a759904e627d03dc49a41a027dde8b0
andrebarrocas <andrebarrocas99@hotmail.com>
2020-05-15 19:11:38 +0100

    Adicionados objetos terra e lua e texturas

---
+++ Terra.cpp
@@ -0,0 +1,74 @@
+#include "Planeta.h"
+#include <stdexcept>
+#define STB_IMAGE_IMPLEMENTATION
+GLint Planeta::width=0;
+GLint Planeta::height=0;
+unsigned int Planeta::texture=0;
+
+
+
+void Planeta::inicializarTextura(){
+
+    int n;
+    unsigned char *dados = stbi_load("img/saturno.jpg", &width, &height, &n, 0);
+    printf("%d %d\n",width,height);
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, dados);
+    gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGB, width, height, GL_RGB, GL_UNSIGNED_BYTE, dados);
+    stbi_image_free(dados);
+
+}
+
+Planeta::Planeta(Point &center):center(center){
+    pattern_buffer = 0; 
+    glGenBuffers(1, &pattern_buffer);	
+	glBindBuffer(GL_ARRAY_BUFFER, pattern_buffer);		
+
+    bool res = loadObj("objs/planeta_com_anel.obj", &points, &uv,&normals,&f);
+
+  
+    glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(glm::vec3), points.data(), GL_STATIC_DRAW);
+    if(!res){
+        throw std::runtime_error("Error loading object");
+    }
+}
+
+Planeta::~Planeta(){
+    glDeleteBuffers(1,&pattern_buffer);
+}
+
+void Planeta::drawShape(){
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glEnable(GL_TEXTURE_2D);
+    glBegin(GL_TRIANGLES);
+
+  
+    for (int i = 0; i < (int)points.size(); i++)
+    {
+
+        glTexCoord2f(uv[i].x, uv[i].y);
+        
+        glNormal3f(normals[i].x, normals[i].y, normals[i].z);
+        
+        glVertex3f(points[i].x, points[i].y, points[i].z); 
+    }
+
+    glEnd();
+    glDisable(GL_TEXTURE_2D);  
+
+}
+
+const char* Planeta::getLabel(){
+    return "Planeta";
+}
+
+void Planeta::Update(){
+
+
+}
