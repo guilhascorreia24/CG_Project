c06667e93333e21a5afb42efeb23061f52f1b6f7
Miguel <miguelmartins17@hotmail.com>
2020-05-20 23:02:04 +0100

    fogeutao

--- World.cpp
+++ World.cpp
@@ -17,10 +17,10 @@ void World::fillObjects(){
 
     Point dist_point(0,0,0);
     // Point foguetao_point(0,0,-5);
-    Point nave_point(0,0,-25);
-    Point planet_point(0,0,-30);
-    Point satelite_point(0,0,-50);
-    Point sol_point(20,0,-200);
+    Point nave_point(0,0,0);
+    Point planet_point(0,0,-180);
+    Point satelite_point(0,0,-150);
+    Point sol_point(20,0,-100);
     // Point terra_point(0,0,0);
     // Point lua_point(0,0,-120);
 
@@ -162,12 +162,13 @@ void World::fillObjects(){
 
 
     Foguetao* foguetao = new Foguetao(foguetao_point);
+    foguetao->disablePhisycs();
     foguetao->setPosition(foguetao_point);
     foguetao->setVelocity(0.3);
     //foguetao->setUniformOrbit(sol,0.9,1);
     foguetao->setSizeObject();
-    Vector direcao (1,1,1);
-    foguetao->setDirection(direcao);
+    //Vector direcao (0.0,1,0.0);
+    //foguetao->setDirection(direcao);
 
 //--------------------------------------------------------------------
 
